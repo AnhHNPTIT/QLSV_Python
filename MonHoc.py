24e1882ab3b189cedd32405a6e7241332f556fc0
lananhlala <lananhlala31@gmail.com>
2022-01-08 22:12:56 +0700

    code

--- MonHoc.py
+++ MonHoc.py
@@ -3,19 +3,6 @@ class MonHoc:
         self._ma = ma
         self._ten = ten
         self._so_tin_chi = so_tin_chi
-
-    def getMa(self):
+    
+    def get_ma(self):
         return self._ma
-
-    def getTen(self):
-        return self._ten
-
-    def getSoTinChi(self):
-        return self._so_tin_chi
-
-    @classmethod
-    def nhap(cls):
-        ma = input("Nhap ma mon hoc: ")
-        ten = input("Nhap ten mon hoc: ")
-        so_tin_chi = input("Nhap so tin chi: ")
-        return MonHoc(ma, ten, so_tin_chi)
